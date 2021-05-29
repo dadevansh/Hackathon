7cf29928d55b27fceca6cfd4f63f96bd7c385878
Devansh <daa.devansh@gmail.com>
2021-05-30 01:45:40 +0530

    Signup

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login';
 import '../../App.css';
 
-export default function SignUp() {
-  return <h1 className='sign-up'>Sign up here</h1>;
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      name: "",
+      city: "",
+      state: "",
+      contact: ""
+    }
+  }
+  render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+      this.setState({
+        email: response.profileObj.email,
+        name: response.profileObj.givenName + " " + response.profileObj.familyName
+
+      })
+    };
+
+    return (
+      <div>
+        <GoogleLogin
+          className="google-btn"
+          clientId="876716683941-bhnmk3o2g18vkh00t8iop0g08b3f9hnp.apps.googleusercontent.com"
+          buttonText="Sign up with Google"
+          onSuccess={responseGoogle}
+          onFailure={() => { }}
+          cookiePolicy={'single_host_origin'}
+        />
+      </div>
+    );
+  }
 }
+export default SignUp;
