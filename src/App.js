4e69eb08b4269713f9b90738fc4917b0b96886ce
Gaurav Chhapliyal <gauravchhapliyal2001@gmail.com>
2021-05-30 02:07:17 +0530

    Front page edited

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/pages/Home'
 import Services from './components/pages/Services'
 import Products from './components/pages/Products'
 import SignUp from './components/pages/SignUp'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
